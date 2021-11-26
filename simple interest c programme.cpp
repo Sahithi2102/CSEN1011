732be55d9aa777799e39f7f6292635174e5dea29
Sahithi <93367910+Sahithi2102@users.noreply.github.com>
2021-11-26 11:30:35 +0530

    Add files via upload

---
+++ simple interest c programme.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() 
+{
+    int p,t,r;
+    
+    printf("principle=");
+    scanf("%d", &p);
+     
+    printf("Rate of interest=");
+    scanf("%d", &r);
+     
+    printf("Time period=");
+    scanf("%d", &t);
+    printf("simple interest= %d \n", (p*t*r\100);  
+     return 0;	
+}
+
+
